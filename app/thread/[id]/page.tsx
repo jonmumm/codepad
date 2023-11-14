83db9d61f26679aa0e7e73b11958737ac9b7eb41
Jonathan Mumm <jon@explorers.club>
2023-11-14 09:35:30 -0800

    more styling

--- app/thread/[id]/page.tsx
+++ app/thread/[id]/page.tsx
@@ -26,8 +26,21 @@ import { assert } from "@/lib/utils";
 import { User } from "@/party/utils/auth";
 import { getServerSession } from "next-auth";
 import { getThread } from "../requests";
-import { ShareIcon } from "lucide-react";
+import { ShareIcon, Users2Icon } from "lucide-react";
 import { Share } from "./components.client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const thread = getThread(params.id);
@@ -37,18 +50,41 @@ export default async function Page({ params }: { params: { id: string } }) {
   const user = session?.user as User | null;
 
   return (
-    <>
+    <div className="flex flex-col gap-3 flex-1">
       <Card className="w-full flex-none">
-        <Share host={headers().get("host")!} id={params.id} />
+        <div className="">
+          <Share host={headers().get("host")!} id={params.id} />
+        </div>
       </Card>
-      <Card className="w-full flex-2">
+      <div className="w-full flex-2">
         <div className="flex-1 flex flex-row gap-2 w-full">
           <Card className="flex-1">
-            {/* <Chat
-            initialUser={user}
-            initialMessages={chat.messages}
-            interviewId={params.id}
-          /> */}
+            <CardHeader className="flex flex-row gap-1">
+              <div className="flex-1">
+                <CardTitle>Chat</CardTitle>
+                <CardDescription>With the AI and friends</CardDescription>
+              </div>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button variant="ghost">
+                    <Users2Icon />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent>
+                  <Command>
+                    {/* <CommandInput></CommandInput> */}
+                    {/* <CommandInput placeholder="Search participants"></CommandInput> */}
+                    <CommandList>
+                      <CommandGroup>
+                        <CommandEmpty>Empty</CommandEmpty>
+                        <CommandItem>Jon</CommandItem>
+                        <CommandItem>Bakery</CommandItem>
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>
+            </CardHeader>
           </Card>
           <Card className="flex-1">
             <CardHeader>
@@ -60,10 +96,15 @@ export default async function Page({ params }: { params: { id: string } }) {
             <Editor interviewId={params.id} />
           </Card>
         </div>
+      </div>
+      <Card className="flex flex-1">
+        <CardHeader>
+          <CardTitle></CardTitle>
+        </CardHeader>
+        <div className="flex-1 flex items-center justify-center">
+          <div id="drop-area">Drop files or paste images here</div>
+        </div>
       </Card>
-      <Card className="w-full flex-1">
-        <div className="h-full">Canvas</div>
-      </Card>
-    </>
+    </div>
   );
 }
